4c5100b8dd2f6da61a16000d5f66205d6ac78db2
tufanpandu <tufan4344@gmail.com>
2023-12-24 00:31:51 +0530

    Remove all file deleted file

--- MERN GitHub/JavaScript/Assignment/Assignment-1/highestMark.js
+++
@@ -1,9 +0,0 @@
-// Create an array
-const mark = [42 ,50 , 95 , 35 , 55]
-
-//Ternary Operator
-// Approch -1
-let highestMark =  Math.max(...mark);
-console.log(highestMark);
-
-//approch -2 
--- test.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello Tufan");
-console.log("modifiled")
