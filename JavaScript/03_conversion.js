7a73d3a4544c8d0b4c422104f3ec613773dcfd1b
tufanpandu <tufan4344@gmail.com>
2024-01-03 00:56:39 +0530

    Conversion

---
+++ JavaScript/03_conversion.js
@@ -0,0 +1,13 @@
+// "33 " => String
+// "33abc" = > NaN
+// 1 => true
+// 0 => false
+
+let num = 33;
+console.log(String(num)); // Number to string conversion
+let age = "25"
+console.log(Number(age)); // String to Number conversion
+let isLogedIn = 1
+console.log(Boolean(isLogedIn));// true
+ isLogedIn = 0; // false
+ isLogedIn = "" // false
