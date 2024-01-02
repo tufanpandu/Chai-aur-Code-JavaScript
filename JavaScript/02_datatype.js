c40a1cfd3c2afa5cdba3333b625177e11dfe5851
tufanpandu <tufan4344@gmail.com>
2024-01-03 00:35:27 +0530

    change path

---
+++ JavaScript/02_datatype.js
@@ -0,0 +1,18 @@
+// "use strict" // treat all JS code as newer version.
+
+// alert("hello world")  // it's work in browser  here we used node.js
+
+// Type of datatype (total 8 datatype)
+//   premitive datatype
+let name = "Tufan" // Stirng
+let age = 25 // Number
+//bigint
+//boolean
+//null  -this is an `object` (It's represent the empty velue)
+//undefined 
+//symbol
+
+//object
+
+
+console.log(typeof null); // output : object
