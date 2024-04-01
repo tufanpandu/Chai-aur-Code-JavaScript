6b821b1f7aa972f818908308cc924b700fe2f5c5
tufanpandu <tufan4344@gmail.com>
2024-04-01 23:01:42 +0530

    Object notes

--- JavaScript/02_Basic_JS/03_object.js
+++ JavaScript/02_Basic_JS/03_object.js
@@ -22,6 +22,7 @@ const regularUser ={
 }
 
 console.log(regularUser.fullname.userFullName.firstName);
+// console.log(regularUser.fullname?.userFullName.firstName);
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "c", 4: "d"}
